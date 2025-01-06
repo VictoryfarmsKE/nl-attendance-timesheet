74ededa1092dc9144265459603274ec29ab4a9a5
Sahil <sahilsuthar607@gmail.com>
2025-01-06 23:30:14 +0530

    chore: calculation added for payroll setting

--- nl_attendance_timesheet/nl_attendance_timesheet/doctype/navari_custom_payroll_settings/navari_custom_payroll_settings.py
+++ nl_attendance_timesheet/nl_attendance_timesheet/doctype/navari_custom_payroll_settings/navari_custom_payroll_settings.py
@@ -1,8 +1,12 @@
 # Copyright (c) 2023, Navari Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class NavariCustomPayrollSettings(Document):
-	pass
+	def validate(self):
+		for row in self.new_bonus_vs_score_matrix:
+			row.lower_limit = row.lower_limit_score * 20
+			row.upper_limit = row.upper_limit_score * 20
+			row.attained_score = row.attained_result_score * 20
