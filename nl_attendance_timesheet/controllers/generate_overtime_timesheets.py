4da8f292ab0522315c57d34c0f2736a16c6113c2
Sahil <sahilsuthar607@gmail.com>
2025-01-15 00:44:35 +0530

    fix: holiday list changes added for timesheet generation

--- nl_attendance_timesheet/controllers/generate_overtime_timesheets.py
+++ nl_attendance_timesheet/controllers/generate_overtime_timesheets.py
@@ -4,6 +4,7 @@ from frappe.utils.data import get_datetime, nowdate
 from datetime import datetime
 from pypika import Criterion
 from datetime import timedelta
+from erpnext.accounts.utils import get_fiscal_year
 
 current_date = nowdate()
 
@@ -38,10 +39,8 @@ def generate_overtime_timesheets(start_date=current_date, end_date=current_date)
 		attendance.in_time.as_("in_time"),
 		attendance.out_time.as_("out_time"),
 		attendance.working_hours.as_("working_hours"),
-		employee.holiday_list.as_("holiday_list"),
 		employee.company.as_("company"),
 		employee.department.as_("department"),
-		employee.holiday_list.as_("holiday_list"),
 		shift_type.start_time.as_("shift_start_time"),
 		shift_type.end_time.as_("shift_end_time"),
 		shift_type.min_hours_to_include_a_break,
@@ -50,9 +49,16 @@ def generate_overtime_timesheets(start_date=current_date, end_date=current_date)
 
 	attendance_records = query.run(as_dict=True)
 
+	holiday_data = frappe._dict()
+
 	for entry in attendance_records:
-		if entry.get('holiday_list'):
-			holiday_dates = frappe.db.get_all('Holiday', filters={'parent': entry.holiday_list}, pluck='holiday_date')
+		if not holiday_data.get(entry.employee):
+			date = entry.in_time.date() or entry.out_time.date()
+			if not fiscal_year:
+				fiscal_year = get_fiscal_year(date)[0]
+			holiday_data[entry.employee] = frappe.db.get_value("Holiday List", {"custom_employee": entry.employee, "custom_fiscal_year": None})
+		if holiday_data.get(entry.employee):
+			holiday_dates = frappe.db.get_all('Holiday', filters={'parent': holiday_data[entry.employee]}, pluck='holiday_date')
 			if entry.attendance_date in holiday_dates:
 				total_work_duration = calculate_holiday_hours(entry)
 				if total_work_duration:
